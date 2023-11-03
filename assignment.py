275a253602778eaf8dd6e78a01226f3751a1085d
DezEv <144241746+DezEv@users.noreply.github.com>
2023-11-03 13:05:13 +0000

    YEs

--- assignment.py
+++ assignment.py
@@ -1,1 +1,20 @@
+print("Welcome to Jake's Pizzeria!")
+print("Here are our sizes.")
+print("1. Large | $6.00 ")
+print("2. Extra Large | $10.00")
+size = input("What sized pizza would you like?")
 
+print("Toppings")
+print("A. 1 Topping | $1.00 ")
+print("B. 2 Toppings | $1.75")
+print("C. 3 Toppings | $2.50")
+print("D. 4 Toppings | $3.35 ")
+toppings = input("How many toppings would you like on your pizza")
+
+LargeP = 6.00
+XLargeP = 10.00
+
+topping1 = 1.00
+topping2 = 1.75
+topping3 = 2.50
+topping4 = 3.35
