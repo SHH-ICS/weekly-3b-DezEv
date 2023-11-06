6e60130913d1cb83b9062562d5e0954a37e27631
DezEv <144241746+DezEv@users.noreply.github.com>
2023-11-06 14:07:57 +0000

    hhh

--- assignment.py
+++ assignment.py
@@ -1,20 +1,40 @@
-print("Welcome to Jake's Pizzeria!")
-print("Here are our sizes.")
-print("1. Large | $6.00 ")
-print("2. Extra Large | $10.00")
-size = input("What sized pizza would you like?")
+print("The Bob Pizzeria")
+print("Here are our pizza options")
+print("Large Pizza | $6.00")
+print("Extra Large pizza | $10.00")
+print("Toppings: 1 = $1.00, 2 = $1.75, 3 = $2.50, 4 = $3.35")
+print("Order")
+size = input("Which pizza would you like")
 
-print("Toppings")
-print("A. 1 Topping | $1.00 ")
-print("B. 2 Toppings | $1.75")
-print("C. 3 Toppings | $2.50")
-print("D. 4 Toppings | $3.35 ")
-toppings = input("How many toppings would you like on your pizza")
+pizzaprice = [6.0,10.0]
+toppingprices = [1.0, 1.75, 2.5, 3.35]
+subtotal = 0.00
 
-LargeP = 6.00
-XLargeP = 10.00
+if str(size) == 'L' or str(size) == 'XL':
+    toppings = input("How many toppings? Type a number 1-4")
+    if int(toppings) > 4:
+        print("Please choose 1-4 toppings")
+    elif int(toppings) < 1:
+        print("Please choose 1-4 toppings")
+    else:
+        print("You have chosen", size, "pizza and", toppings, "toppings!")
+        if str(size) == 'L':
+            subtotal = pizzaprice[0]
+        elif str(size) == 'XL':
+            subtotal = pizzaprice[1]
 
-topping1 = 1.00
-topping2 = 1.75
-topping3 = 2.50
-topping4 = 3.35
+        if int(toppings) == 1:
+            subtotal = subtotal + toppingprices[0]
+        elif int(toppings) == 2:
+            subtotal = subtotal + toppingprices[1]
+        elif int(toppings) == 3:
+            subtotal = subtotal + toppingprices[2]
+        elif int(toppings) == 4:
+            subtotal = subtotal + toppingprices[3]
+else:
+    print("Please choose either a L or XL pizza.")
+
+taxes = float(subtotal) * 0.13
+total_price = float(subtotal) + taxes
+print("Your order summary:")
+print("Sub total: $",format(subtotal, ".2f"), "Taxes: $",format(taxes, ".2f"), "Amount due: $",format(total_price, ".2f"))
